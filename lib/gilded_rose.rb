1c565dff4c4d0fdc7e58d77d95891042c981c92a
Adam Lodowski <adamlodowski@gmail.com>
2019-02-15 16:17:13 +0000

    Refactor: Extracted backstage passes to own method. Tests passing again

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,43 +11,30 @@ class GildedRose
       if sulfuras?(item)
       elsif aged_brie?(item)
         item.quality = update_aged_brie(item.quality)
+      elsif backstage_passes?(item)
+        item.quality = update_backstage_passes(item.quality, item.sell_in)
       else
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-          item.quality = reduce_quality(item.quality)
-        else
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-          item.sell_in = item.sell_in - 1
+        item.quality = reduce_quality(item.quality)
+      end
+      item.sell_in = item.sell_in - 1 unless sulfuras?(item)
         
         
-          if item.sell_in < 0
-          if item.name != "Aged Brie"
-            if item.name != "Backstage passes to a TAFKAL80ETC concert"
-              if item.quality > 0
-                  item.quality = item.quality - 1
-              end
-            else
-              item.quality = item.quality - item.quality
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              item.quality = item.quality - 1
             end
           else
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
           end
         end
       end
-   end
+    end
   end
 
   def sulfuras?(item)
@@ -64,6 +51,23 @@ class GildedRose
 
   def update_aged_brie(item_quality)
     item_quality < 50 ? item_quality + 1 : item_quality
+  end
+
+  def backstage_passes?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def update_backstage_passes(item_quality, item_sell_in)
+    if item_sell_in < 0
+      0
+    elsif item_sell_in.between?(0, 5)
+      item_quality + 3
+    elsif item_sell_in.between?(0, 10)
+      item_quality + 2
+    else
+      item_quality < 50 ? item_quality + 1 : item_quality
+    end
+
   end
 
 end
