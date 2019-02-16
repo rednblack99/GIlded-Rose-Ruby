661a521b7deb0896eedbe951faa000a38605c382
Adam Lodowski <adamlodowski@gmail.com>
2019-02-16 18:18:09 +0000

    Removed magic numbers

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,6 +2,13 @@ require_relative './item'
 
 class GildedRose
 
+  REDUCE_NORMAL = 1
+  REDUCE_DOUBLE = 2
+  INCREASE_NORMAL = 1
+  INCREASE_DOUBLE = 2
+  INCREASE_TRIPLE = 3
+
+
   def initialize(items)
     @items = items
   end
@@ -15,7 +22,7 @@ class GildedRose
       else
         item.quality = reduce_quality(item.quality) unless sulfuras?(item)
       end
-      item.sell_in = item.sell_in - 1 unless sulfuras?(item)
+      item.sell_in = item.sell_in - REDUCE_NORMAL unless sulfuras?(item)
     end
   end
 
@@ -27,9 +34,9 @@ class GildedRose
 
   def reduce_quality(item_quality)
     if item_quality.negative?
-      item_quality - 2
+      item_quality - REDUCE_DOUBLE
     else
-      item_quality.between?(1, 50) ? item_quality - 1 : item_quality
+      item_quality.between?(1, 50) ? item_quality - REDUCE_NORMAL : item_quality
     end
   end
 
@@ -39,9 +46,9 @@ class GildedRose
 
   def update_aged_brie(item_quality, item_sell_in)
     if item_sell_in.negative?
-      item_quality + 2 unless item_quality + 2 > 50
+      item_quality + INCREASE_DOUBLE unless item_quality + INCREASE_DOUBLE > 50
     else
-      item_quality < 50 ? item_quality + 1 : item_quality
+      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
     end
   end
 
@@ -52,12 +59,11 @@ class GildedRose
   def update_backstage_passes(item_quality, item_sell_in)
     return 0 if item_sell_in.negative?
     if item_sell_in.between?(0, 5)
-      item_quality + 3
+      item_quality + INCREASE_TRIPLE
     elsif item_sell_in.between?(0, 10)
-      item_quality + 2
+      item_quality + INCREASE_DOUBLE
     else
-      item_quality < 50 ? item_quality + 1 : item_quality
+      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
     end
   end
-
 end
