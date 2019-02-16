ca280463d61045634d4e251a85247dcd156825df
Adam Lodowski <adamlodowski@gmail.com>
2019-02-16 19:14:02 +0000

    Extracted aged brie to own class

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -4,8 +4,18 @@ class AgedBrie
 
   include Constants
 
-  def initialize(item)
-    @item = item
+  def initialize(item_quality, item_sell_in)
+    @item_quality = item_quality
+    @item_sell_in = item_sell_in
+  end
+
+  def determine_quality_change
+    if @item_sell_in.negative?
+      @item_quality + INCREASE_DOUBLE unless @item_quality + INCREASE_DOUBLE > 50
+    else
+      @item_quality < 50 ? @item_quality + INCREASE_NORMAL : @item_quality
+    end
+
   end
 
 end
--- lib/update_quality.rb
+++ lib/update_quality.rb
@@ -12,7 +12,7 @@ class UpdateQuality
 
   def determine_quality_change
     if aged_brie?(@item)
-      update_aged_brie(@item.quality, @item.sell_in)
+      AgedBrie.new(@item.quality, @item.sell_in).determine_quality_change
     elsif backstage_passes?(@item)
       BackstagePasses.new(@item.quality, @item.sell_in).determine_quality_change
     else
@@ -34,26 +34,7 @@ class UpdateQuality
     item.name == "Aged Brie"
   end
 
-  def update_aged_brie(item_quality, item_sell_in)
-    if item_sell_in.negative?
-      item_quality + INCREASE_DOUBLE unless item_quality + INCREASE_DOUBLE > 50
-    else
-      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
-    end
-  end
-
   def backstage_passes?(item)
     item.name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-
-  def update_backstage_passes(item_quality, item_sell_in)
-    return 0 if item_sell_in.negative?
-    if item_sell_in.between?(0, 5)
-      item_quality + INCREASE_TRIPLE
-    elsif item_sell_in.between?(0, 10)
-      item_quality + INCREASE_DOUBLE
-    else
-      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
-    end
   end
 end
