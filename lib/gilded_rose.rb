16af403eba8d85dd81bc5d233c1bb8af5844ddf1
Adam Lodowski <adamlodowski@gmail.com>
2019-02-16 19:01:44 +0000

    Refactor: extracted update quality methods to own class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,6 @@
 require_relative './item'
 require_relative './constants'
+require_relative './update_quality'
 
 class GildedRose
 
@@ -11,13 +12,8 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if aged_brie?(item)
-        item.quality = update_aged_brie(item.quality, item.sell_in)
-      elsif backstage_passes?(item)
-        item.quality = update_backstage_passes(item.quality, item.sell_in)
-      else
-        item.quality = reduce_quality(item.quality) unless sulfuras?(item)
-      end
+      quality_change = UpdateQuality.new(item).determine_quality_change
+      item.quality = quality_change unless sulfuras?(item)
       item.sell_in = item.sell_in - REDUCE_NORMAL unless sulfuras?(item)
     end
   end
@@ -26,40 +22,5 @@ class GildedRose
 
   def sulfuras?(item)
     item.name == "Sulfuras, Hand of Ragnaros"
-  end
-
-  def reduce_quality(item_quality)
-    if item_quality.negative?
-      item_quality - REDUCE_DOUBLE
-    else
-      item_quality.between?(1, 50) ? item_quality - REDUCE_NORMAL : item_quality
-    end
-  end
-
-  def aged_brie?(item)
-    item.name == "Aged Brie"
-  end
-
-  def update_aged_brie(item_quality, item_sell_in)
-    if item_sell_in.negative?
-      item_quality + INCREASE_DOUBLE unless item_quality + INCREASE_DOUBLE > 50
-    else
-      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
-    end
-  end
-
-  def backstage_passes?(item)
-    item.name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-
-  def update_backstage_passes(item_quality, item_sell_in)
-    return 0 if item_sell_in.negative?
-    if item_sell_in.between?(0, 5)
-      item_quality + INCREASE_TRIPLE
-    elsif item_sell_in.between?(0, 10)
-      item_quality + INCREASE_DOUBLE
-    else
-      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
-    end
   end
 end
---
+++ lib/update_quality.rb
@@ -0,0 +1,57 @@
+require_relative './constants'
+
+class UpdateQuality
+
+  include Constants
+
+  def initialize(item)
+    @item = item
+  end
+
+  def determine_quality_change
+    if aged_brie?(@item)
+      update_aged_brie(@item.quality, @item.sell_in)
+    elsif backstage_passes?(@item)
+      update_backstage_passes(@item.quality, @item.sell_in)
+    else
+      reduce_quality(@item.quality)
+    end
+  end
+
+  private
+
+  def reduce_quality(item_quality)
+    if item_quality.negative?
+      item_quality - REDUCE_DOUBLE
+    else
+      item_quality.between?(1, 50) ? item_quality - REDUCE_NORMAL : item_quality
+    end
+  end
+
+  def aged_brie?(item)
+    item.name == "Aged Brie"
+  end
+
+  def update_aged_brie(item_quality, item_sell_in)
+    if item_sell_in.negative?
+      item_quality + INCREASE_DOUBLE unless item_quality + INCREASE_DOUBLE > 50
+    else
+      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
+    end
+  end
+
+  def backstage_passes?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def update_backstage_passes(item_quality, item_sell_in)
+    return 0 if item_sell_in.negative?
+    if item_sell_in.between?(0, 5)
+      item_quality + INCREASE_TRIPLE
+    elsif item_sell_in.between?(0, 10)
+      item_quality + INCREASE_DOUBLE
+    else
+      item_quality < 50 ? item_quality + INCREASE_NORMAL : item_quality
+    end
+  end
+end
