180ea70bbee99f4d9c6b5a796e0948e2dafa39be
Adam Lodowski <adamlodowski@gmail.com>
2019-02-16 18:20:00 +0000

    Extracted constants to separate module

---
+++ lib/constants.rb
@@ -0,0 +1,9 @@
+module Constants
+
+  REDUCE_NORMAL = 1
+  REDUCE_DOUBLE = 2
+  INCREASE_NORMAL = 1
+  INCREASE_DOUBLE = 2
+  INCREASE_TRIPLE = 3
+
+end
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,13 +1,9 @@
 require_relative './item'
+require_relative './constants'
 
 class GildedRose
 
-  REDUCE_NORMAL = 1
-  REDUCE_DOUBLE = 2
-  INCREASE_NORMAL = 1
-  INCREASE_DOUBLE = 2
-  INCREASE_TRIPLE = 3
-
+  include Constants
 
   def initialize(items)
     @items = items
